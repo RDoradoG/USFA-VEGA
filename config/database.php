4748917f96b4553987513ce8b3a5be79d4dfc059
Rodrigo Dorado <dorado.rodrigo.rd@gmail.com>
2026-07-11 10:39:21 -0400

    Set version of the app

--- config/database.php
+++ config/database.php
@@ -99,7 +99,23 @@ return [
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
+        //Dev
         'sqlsrv' => [
+            'driver' => 'sqlsrv',
+            //'url' => env('DB_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', ''),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'encrypt' => 'yes',
+            'trust_server_certificate' => true,
+        ],
+
+        //Prod
+        /*'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', 'localhost'),
@@ -110,9 +126,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
-        ],
+        ],*/
 
     ],
 
--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { usePage } from '@inertiajs/vue3'
+
+// Obtener props compartidos desde Laravel
+const page = usePage()
+const version = page.props.app_version
+
 </script>
 
 <template>
@@ -9,8 +15,7 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
         <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold"
-            >USFA - VEGA</span
-        >
+        <span class="mb-0.5 truncate leading-tight font-semibold">USFA - VEGA</span>
+        <span class="text-xs text-gray-400 leading-tight">V {{ version }}</span>
     </div>
 </template>
